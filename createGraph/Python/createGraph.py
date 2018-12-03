9613e500929a7a91f5fe9f08dee575cae5845582
Guilherme <guinbsa@gmail.com>
2018-12-02 23:44:38 -0200

    add createFile function

--- createGraph/Python/createGraph.py
+++ createGraph/Python/createGraph.py
@@ -1,4 +1,12 @@
 import sys
+import os
+
+def createFile(filePath):
+	fileName=filePath.split('/')[len(filePath.split('/'))-1]
+	if not os.path.exists(filePath.replace(fileName, '')):
+		os.makedirs(filePath.replace(fileName, ''))
+	file=open(filePath, 'w')
+	return file
 
 def searchParameter(parameter, args):
 	flag=False
@@ -10,13 +18,18 @@ def searchParameter(parameter, args):
 	help()
 
 def help():
-	print('{0} -t <tam_graph> -p <path_dir>\ntam_graph min = 13'.format(sys.argv[0]))
+	if sys.argv[0]=='createGraph.py':
+		print('{0} -t <tam_graph> -p <path_dir>\tor\tcreateGraph(<tam_graph>, <path_dir>) in another python3 file\ntam_graph min = 13'.format(sys.argv[0]))
+	else:
+		print('createGraph(<tam_graph>, <path_dir>)\ntam_graph min = 13')
 	exit(0)
 
 def createGraph(t, p):
 	if t<13:
 		help()
-	pass
+	file=createFile(p)
+	#TODO
+	file.close()
 
 if __name__=='__main__':
 	if len(sys.argv)==5:
